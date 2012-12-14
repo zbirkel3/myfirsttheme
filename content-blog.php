1202aa50ad0a08efd273eb19b56af64f04cbbd6f
unknown <zbirkel854@wit-xdvm-045.MILLARDPS.NET>
2012-12-14 11:29:05 -0600

    added css styling and comments

--- content-blog.php
+++ content-blog.php
@@ -1,13 +1,15 @@
 <?php if(have_posts()): ?>
 	<?php while (have_posts()) : the_post() ?>
-		<h2><?php the_title()?></h2>
-		<div class="meta">
-			<span class="author">by <?php the_author()?></span>
-			<span class="date">Published on <?php the_time(get_option('date_format')); ?></span>
-			<span class="comments">  </span>
-		</div>
-		<div class="story-content">
-			<?php the_content()?>
+		<div class="post">
+			<h2><a href="<?php the_permalink() ?>"><?php the_title()?></a></h2>
+			<div class="meta">
+				<span class="author">by <?php the_author()?></span>
+				<span class="date">Published on <?php the_time(get_option('date_format')); ?></span>
+				<span class="comments"> <?php comments_number('no comments', 'one comment' , '%comments'); ?> </span>
+			</div>
+			<div class="story-content">
+				<?php the_content()?>
+			</div>
 		</div>
 	<?php endwhile;?>
 <?php endif;?>
--- content-single.php
+++ content-single.php
@@ -1,8 +1,16 @@
 <?php if(have_posts()): ?>
 	<?php while (have_posts()) : the_post() ?>
-		<h2><?php the_title()?></h2>
-		<div class="story-content">
-			<?php the_content() ?>
+		<div>
+			<h2><?php the_title()?></h2>
+				<div class="meta">
+					<span class="author">by <?php the_author()?></span>
+					<span class="date">Published on <?php the_time(get_option('date_format')); ?></span>
+					<span class="comments"> <?php comments_number('no comments', 'one comment' , '%comments'); ?> </span>
+				</div>
+				<div class="story-content">
+					<?php the_content()?>
+				</div>
 		</div>
+		<?php comments_template()?>
 	<?php endwhile;?>
 <?php endif;?>
