bcd7746d37227679b9752884b8a429ab6365a4ca
unknown <zbirkel854@wit-xdvm-030.MILLARDPS.NET>
2012-12-19 11:25:00 -0600

    changes

--- header.php
+++ header.php
@@ -1,7 +1,9 @@
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri()?>/bootstrap/css/bootstrap.min.css" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url')?>" />
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri()?>/bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri()?>/myfirsttheme.js"></script>
 
 <title><?php bloginfo('name');?></title>
--- myfirsttheme.js
+++ myfirsttheme.js
@@ -1,33 +1,37 @@
 //execute jQUery functions only after jQuery JS has loaded
 $(function (){
-	$('#main .row .span9').click(function(){
-		$(this).addClass('');
-	});
-	
-	$('#main .row .span3').hover(
-			function(){
-				$(this).addClass('highlight');
-			},
-			function(){
-				$(this).removeClass('highlight');
-			}
-	);
-	
-	//capture double click of paragraphs
-	$('.story-content p').dblclick(function(){
-		$(this).fadeOut(1000);
-	});
-
-	//listen for key up events
-	$(document).keyup(function(e){
-		switch(e.which){
-		case 76:// l
-			// get the url for wp-admin
-			var url = $('footer a.login').attr('href');
-			window.location = url;
-			break;	
-		}
-	}
-)}
-);
-	
+	//add bootstrap affix to nav
+	$('#primary-nav').attr('data-spy','affix');
+	$('#primary-nav').attr('data-offset-top','100');
+});	
+//	$('#main .row .span9').click(function(){
+//		$(this).addClass('');
+//	});
+//	
+//	$('#main .row .span3').hover(
+//			function(){
+//				$(this).addClass('highlight');
+//			},
+//			function(){
+//				$(this).removeClass('highlight');
+//			}
+//	);
+//	
+//	//capture double click of paragraphs
+//	$('.story-content p').dblclick(function(){
+//		$(this).fadeOut(1000);
+//	});
+//
+//	//listen for key up events
+//	$(document).keyup(function(e){
+//		switch(e.which){
+//		case 76:// l
+//			// get the url for wp-admin
+//			var url = $('footer a.login').attr('href');
+//			window.location = url;
+//			break;	
+//		}
+//	}
+//)}
+//);
+//	
